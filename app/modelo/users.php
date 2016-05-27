69095d6f0c1bd8e567a8d20c491501f7a3c9e763
davymtz <davymtz91@gmail.com>
2016-05-27 14:24:21 -0500

    control de login
    control de login perfectisimo, solo hay que hacer el panel para el
    administrador, digo, si es que se va a hacer, sino para que se modifique
    el login

--- app/modelo/users.php
+++ app/modelo/users.php
@@ -7,5 +7,24 @@
 	$user = $mysqli->real_escape_string($user);
 	$pass = $mysqli->real_escape_string($pass);
 
-	$query = $mysqli->query("select * from usuarios where nombre='$user' and password = '$pass'");
+	$result = $mysqli->query("SELECT iduser,nombre,password,area,type FROM users,areas WHERE nombre='$user' and password = '$pass' and fkiduser=idarea");
+	$row = $result->fetch_assoc();
+	$datos = array("nombre"=>$row['nombre'],"password"=>$row['password'],"area"=>$row['area'],
+					"type"=>$row['type']);
+
+	if ($fila = $result->num_rows==1) {
+		session_start();
+		$_SESSION["conectado"] = true;
+		$_SESSION["rango"] = $row['type'];
+		$_SESSION['area'] = $row['area'];
+		$salida = array('mensaje' =>"Bienvenido: al Area de: ".$row['area'],'json'=>$datos,"validate"=>"true");
+		$salida = json_encode($salida);
+		echo $salida;
+	} else {
+		$salida = array('mensaje'=>"Tu usuario y password no existen, corrobora tus datos","validate"=>"false");
+		$salida = json_encode($salida);
+		echo $salida;
+	}
+
+	$mysqli->close();
  ?>
--- app/vista/js/login.js
+++ app/vista/js/login.js
@@ -5,29 +5,56 @@
 
 	$("#btn-submit").on("click",verificar);
 
+
 	function verificar(e){
-		console.log(e);
 		e.preventDefault();
+		var arreglo = $(".form-control");
 		if ($user.val() == "" || $pass.val() == "") {
 			$(".error").show();
-			$user.css({"border-color":"red","box-shadow": "0px 0px 10px 0.5px red"})
-			$pass.css({"border-color":"red","box-shadow": "0px 0px 10px 0.5px red"})
+			$.each(arreglo,function (i,index){
+				if(index.value.length==0){
+					$(index).css({"border-color":"red","box-shadow": "0px 0px 10px 0.5px red"});
+				}
+			});
 		} else {
 			$(".error").hide();
 			$user.css({"border-color": "#66afe9", "box-shadow": "0px 0px 10px 0.5px #66afe9"});
 			$pass.css({"border-color": "#66afe9", "box-shadow": "0px 0px 10px 0.5px #66afe9"});
-			submit($user.val(),$pass.val());
+			ingresar($user.val(),$pass.val());
 		}
 	}
 
-	function submit(usuario,password){
+
+	function ingresar(usuario,password){
 		$.ajax({
 			type: "GET",
-			url: "",
-			cache: false,
-			data: {"usuario":usuario,"password":password}
+			url: "app/modelo/users.php",
+			data: {"usuario":usuario,"password":password},
+			dataType: "json"
 		}).done(function (data){
-			// body...
+			//console.log(data.json);
+			if (data.validate == "false"){
+				$(".modal-content").addClass("shake-vertical shake-constant");
+				setTimeout(function (){
+					$(".modal-content").removeClass("shake-vertical shake-constant");
+					$user.val("");
+					$pass.val("");
+				},800);
+				$(".error").html("<h4>"+data.mensaje+"</h4>").show();
+			}else{
+				$(".error").hide();
+				$user.val("");
+				$pass.val("");
+				if(data.json.type==1){ // 1 = administrador
+					//setTimeout(function (){window.location = "panel";},1500);
+				} else if(data.json.type==2){
+					if(data.json.area=="trauma"){
+						setTimeout(function (){window.location = "traumatologia/pp";},1500);
+					} else {
+						setTimeout(function (){window.location = "pediatria/pp";},1500);
+					}
+				}
+			}
 		}).fail(function (){
 			alert("Error al conectarse con el servidor");
 		});
